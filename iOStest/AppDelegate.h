506d1dc9498e2e99fd4820d04559c52aac4bcbbc
yanglei <harveyyang777@gmail.com>
2018-02-12 22:37:07 +0800

    Initial Commit

---
+++ iOStest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOStest
+//
+//  Created by Harvey Yang on 2018/2/12.
+//  Copyright © 2018年 Harvey Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOStest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOStest
+//
+//  Created by Harvey Yang on 2018/2/12.
+//  Copyright © 2018年 Harvey Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
