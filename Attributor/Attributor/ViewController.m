de76fa21d350b6e31d737d1d8d519be3ae408371
yanglei <harveyyang777@gmail.com>
2018-03-12 23:29:44 +0800

    tune into notifications

--- Attributor/Attributor/ViewController.m
+++ Attributor/Attributor/ViewController.m
@@ -11,6 +11,7 @@
 @interface ViewController ()
 @property (strong, nonatomic) IBOutlet UILabel *headline;
 @property (strong, nonatomic) IBOutlet UITextView *body;
+@property (strong, nonatomic) IBOutlet UIButton *outlineButton;
 
 @end
 
@@ -33,19 +34,38 @@
      .textStorage removeAttribute:NSStrokeWidthAttributeName range:self.body.selectedRange];
 }
 
-
-
-
-
-
-
-
-
 - (void)viewDidLoad {
+    NSLog(@"viewdidload");
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSMutableAttributedString *title = [[NSMutableAttributedString alloc]initWithString:self.outlineButton.currentTitle];
+    [title setAttributes:@{ NSStrokeWidthAttributeName:@-3,NSStrokeColorAttributeName:self.outlineButton.tintColor} range:(NSMakeRange(0, [title length]))];
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
 }
 
+-(void) viewWillAppear:(BOOL)animated{
+    NSLog(@"viewWillAppear");
+    [super viewWillAppear:animated];
+    [  [NSNotificationCenter defaultCenter] addObserver:self selector:@selector(preferredFontChanged:) name:UIContentSizeCategoryDidChangeNotification object:nil];
+    
+}
+
+-(void) preferredFontChanged:(NSNotification *)notification{
+    [self usePreferFont];
+    
+}
+
+- (void)usePreferFont{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+}
+
+-(void) viewWillDisappear:(BOOL)animated{
+    NSLog(@"viewWillDisappear");
+    [self viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter]removeObserver:self name:UIContentSizeCategoryDidChangeNotification object:nil];
+}
 
 
 @end
